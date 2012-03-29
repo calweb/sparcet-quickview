65a9e169799ed9b1b3b7350f135e0ae6767fb420
Calvin Webster <calweb@gmail.com>
2012-03-28 20:04:30 -0400

    initial commit

---
+++ js/application.js
@@ -0,0 +1,18 @@
+$(function() {
+    sparcet.initStyling();
+    sparcet.initEvents();
+    sparcet.initValidation();
+});
+
+var sparcet = {
+
+    initStyling: function () {
+
+    },
+
+    initEvents: function () {
+
+    }
+
+
+};
