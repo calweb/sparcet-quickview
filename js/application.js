d622a2d0361581fe4ba76b61b360c92b15bf1a2e
Calvin Webster <calweb@gmail.com>
2012-03-29 01:58:32 -0400

    lots of changes

--- js/application.js
+++ js/application.js
@@ -1,16 +1,47 @@
 $(function() {
     sparcet.initStyling();
     sparcet.initEvents();
-    sparcet.initValidation();
 });
 
 var sparcet = {
 
     initStyling: function () {
+        $.getJSON('https://sparc.sparcet.com/api/sparcets?limit=5', function(results) {
+            var output = '';
+            var $sparcetList = $(".result ul");
+            //var items = results.data;
+            var baseImgURL = "https://sparc.sparcet.com/profiles/"
+
+
+            $.each(results.data, function(index, value) {
+//                output += "<div class='sparcet-detail'><li>"
+//                    +"<img style='width:40px;height:40px;' src='"
+//                    + baseImgURL + this.to.id
+//                    + "/img_Profile' /></li><li>"
+//                    + this.reason + "</li></div>";
+
+               output += "<p class='sparcet-summary'><img class='profile-pic to-user' style='width:40px;height:40px;' src='"
+                    + baseImgURL + this.to.id
+                    + "/img_Profile' alt='"
+                    + this.from.name + "' /><span class='name to-user subject'>"
+                    + this.to.name + "</span> got a sparcet from <span class='name from-user'>"
+                    + this.from.name + "</span></a></p><div class='sparcet-detail'><span class='reason linkscrape'>"
+                    + this.reason +"</span><span class='name from-user'>-"
+                    + this.from.name + "</span><img class='profile-pic from-user' style='width:40px;height:40px;' src='"
+                    + baseImgURL + this.from.id + "/img_Profile' /></div><div style='clear:both'></div>";
+
+                $sparcetList.html(output);
+            });
+
+
+        });
 
     },
 
     initEvents: function () {
+
+
+
 
     }
 
